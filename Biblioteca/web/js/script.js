761825fc73735cbb0639a2175ba71fe95c252c48
Jhosepewe <55572569+Jhosepewe@users.noreply.github.com>
2019-12-03 18:58:47 -0500

    Add files via upload

---
+++ Biblioteca/web/js/script.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+	// AGREGANDO CLASE ACTIVE
+	$('.category_list .category_item[category="all"]').addClass('ct_item-active');
+
+	// FILTRANDO LIBROS
+
+	$('.category_item').click(function(){
+		var catProduct = $(this).attr('category');
+		console.log(catProduct);
+
+		// AGREGANDO CLASE ACTIVE AL ENLACE SELECCIONADO
+		$('.category_item').removeClass('ct_item-active');
+		$(this).addClass('ct_item-active');
+
+		// OCULTAR LIBROS 
+		$('.product-item').css('transform', 'scale(0)');
+		function hideProduct(){
+			$('.product-item').hide();
+		} setTimeout(hideProduct,400);
+
+		// MOSTRANDO LIBROS SELECCIONADOS
+		function showProduct(){
+			$('.product-item[category="'+catProduct+'"]').show();
+			$('.product-item[category="'+catProduct+'"]').css('transform', 'scale(1)');
+		} setTimeout(showProduct,400);
+	});
+
+	// MOSTRANDO TODOS LOS LIBROS
+
+	$('.category_item[category="all"]').click(function(){
+		function showAll(){
+			$('.product-item').show();
+			$('.product-item').css('transform', 'scale(1)');
+		} setTimeout(showAll,400);
+	});
+});
+
+// NO OLVIDAR EDITAR POARA PODER SACAR TODO DE BASE DE DATOS
